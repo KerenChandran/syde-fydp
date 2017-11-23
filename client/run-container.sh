854de9dff0137bace35f6107aa5164d93b728475
Keren Chandran <keren.chandran@gmail.com>
2017-11-23 14:50:32 -0500

    update client docker build script

--- client/run-container.sh
+++ client/run-container.sh
@@ -1,3 +1,5 @@
 # convenience script to run client instance
 
-docker run -d -it -p 3000:3000 --name client client-image
+docker run -d -it -p 3000:3000 -v "$PWD":/App --name client client-image
+
+docker exec -d -it client bash /App/setup.sh
---
+++ client/setup.sh
@@ -0,0 +1,6 @@
+# move node modules from / to src directory
+mv -r node_modules /App/node_modules
+
+cd App
+
+npm start
