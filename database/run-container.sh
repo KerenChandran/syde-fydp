559c36356fd15f0fc30e40c33ccad01e732c572c
Siddhanth Unnithan <sidunnithan@gmail.com>
2018-01-27 13:23:18 -0500

    removing port mapping to allow for trxn database creation

--- database/run-container.sh
+++ database/run-container.sh
@@ -1,3 +1,3 @@
 # convenience script to instantiate postgres container
 
-docker run -d --name db -e POSTGRES_PASSWORD=sydefydp -p 5432:5432 --expose 5432 --net=application-network -v $PWD:/db postgres
+docker run -d --name db -e POSTGRES_PASSWORD=sydefydp --expose 5432 --net=application-network -v $PWD:/db postgres
