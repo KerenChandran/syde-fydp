fe6290213970958e3a242c237566791891a05c68
Keren Chandran <keren.chandran@gmail.com>
2018-02-06 16:50:44 -0500

    compile bootstrap in app

--- client/src/index.js
+++ client/src/index.js
@@ -4,8 +4,8 @@ import { Provider } from 'react-redux'
 import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles';
 
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
 import './css/bootstrap-flat.min.css';
-// import './css/bootstrap-flat-extras.min.css';
 import 'react-bootstrap-table/dist/react-bootstrap-table-all.min.css';
 
 import Router from './router';
