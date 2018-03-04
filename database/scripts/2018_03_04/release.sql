7fc5edb8151bc1d7309a531886e47e330a6d0143
Siddhanth Unnithan <sidunnithan@gmail.com>
2018-03-04 16:50:45 -0500

    renamed existing directories; request and notif schema creation

---
+++ database/scripts/2018_03_04/release.sql
@@ -0,0 +1,40 @@
+-- request and notification related tables
+
+-- primary request table
+CREATE TABLE request (
+    id SERIAL,
+    requester_id SERIAL REFERENCES platform_user (id),
+    resource_id SERIAL REFERENCES resource (id),
+    source_account TEXT,
+    target_account TEXT
+);
+
+-- primary notification table
+CREATE TABLE notification (
+    id SERIAL,
+    message TEXT,
+    read_flag BOOLEAN
+);
+
+-- relationship/mapping tables
+CREATE TABLE request_schedule_blocks (
+    block_id SERIAL,
+    request_id SERIAL REFERENCES request (id),
+    block_start timestamp,
+    block_end timestamp
+);
+
+CREATE TABLE request_incentive (
+    request_id SERIAL REFERENCES request (id),
+    incentive_id SERIAL REFERENCES incentive (id)
+);
+
+CREATE TABLE notification_request (
+    notification_id SERIAL REFERENCES notification (id),
+    request_id SERIAL REFERENCES request (id)
+);
+
+CREATE TABLE user_notification (
+    user_id SERIAL REFERENCES platform_user (id),
+    notification_id SERIAL REFERENCES notification (id)
+);
