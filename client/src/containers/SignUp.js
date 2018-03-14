c6a4ab858962e88f04148f46b8a7e75f6d97e661
juliazhang95 <julia.zhang@live.ca>
2018-03-13 23:51:08 -0400

    styling for edit profile form

--- client/src/containers/SignUp.js
+++ client/src/containers/SignUp.js
@@ -31,36 +31,36 @@ class SignUp extends React.Component {
         } = this.state;
 
         return (
-            <div id="login-page">
-            <div class="container">
-                <div class="loginBox">
-                    <form class="loginContent" onSubmit={this.handleSubmit}>
-                        <div class="row" controlId="formHorizontalEmail">
-                            <label class="control-label">
-                                Email
-                            </label>
-                        </div>
+            <div id="loginPage">
+                <div class="container">
+                    <div class="loginBox">
+                        <form class="loginContent" onSubmit={this.handleSubmit}>
+                            <div class="row" controlId="formHorizontalEmail">
+                                <label class="control-label">
+                                    Email
+                                </label>
+                            </div>
 
-                        <div class="row">
-                                <FormControl type="email" placeholder="Email" value={email} onChange={this.handleChange('email')}/>
-                        </div>
+                            <div class="row">
+                                    <FormControl type="email" placeholder="Email" value={email} onChange={this.handleChange('email')}/>
+                            </div>
 
-                        <div class="row" controlId="formHorizontalPassword">
-                            <label class="control-label">
-                                Password
-                            </label>
-                        </div>
+                            <div class="row" controlId="formHorizontalPassword">
+                                <label class="control-label">
+                                    Password
+                                </label>
+                            </div>
 
-                        <div class="row">
-                                <FormControl type="password" placeholder="Password" value={password} onChange={this.handleChange('password')}/>
-                        </div>
+                            <div class="row">
+                                    <FormControl type="password" placeholder="Password" value={password} onChange={this.handleChange('password')}/>
+                            </div>
 
-                        <div class="row" style={{textAlign: 'center', marginTop: 10+'px'}}>
-                                <button type="submit" class="btn btn-primary">Sign up</button>
-                        </div>
-                    </form>
+                            <div class="row" style={{textAlign: 'center', marginTop: 10+'px'}}>
+                                    <button type="submit" class="btn btn-primary">Sign up</button>
+                            </div>
+                        </form>
+                    </div>
                 </div>
-            </div>
             </div>
 
         )
