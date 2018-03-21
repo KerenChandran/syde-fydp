1eaadc0886f22dae12e02ef943ea67830f78fe95
Keren Chandran <keren.chandran@gmail.com>
2018-03-21 15:45:04 -0400

    Remove balance info from selecting accounts

--- client/src/components/Account/index.js
+++ client/src/components/Account/index.js
@@ -6,6 +6,5 @@ export default ({ id, type, balance, onClick, active }) => (
   <div className={classnames('account-box', { 'active': active })} onClick={onClick(id)}>
     <h4>Account No: {id}</h4>
     <h5>{type === 'research' ? "Research" : "Operational"} Account</h5>
-    <h5>${balance.toFixed(2)}</h5>
   </div>
 );
